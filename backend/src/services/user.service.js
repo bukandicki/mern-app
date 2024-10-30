9fc5cb9b6dd5525434d83ee4f3cfca96bdf8fdb2
Dicki Maulana <bukandicki@gmail.com>
2024-10-30 23:36:10 +0700

    user CRUD service

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,18 +1,39 @@
 const { userModel } = require("../models");
 
-const makeUser = async (payload) => {
+const CREATE = async (payload) => {
   const response = await userModel.create(payload);
 
   return response;
 };
 
-const queryUser = async () => {
+const UPDATE = async (id, payload) => {
+  const response = await userModel.findByIdAndUpdate(id, { ...payload });
+
+  return response;
+};
+
+const DELETE = async (id) => {
+  const response = await userModel.findByIdAndDelete(id);
+
+  return response;
+};
+
+const GET_ALL = async () => {
   const response = await userModel.find();
 
   return response;
 };
 
+const GET_DETAIL = async (email) => {
+  const response = await userModel.findOne({ email });
+
+  return response;
+};
+
 module.exports = {
-  makeUser,
-  queryUser,
+  CREATE,
+  UPDATE,
+  DELETE,
+  GET_ALL,
+  GET_DETAIL,
 };
