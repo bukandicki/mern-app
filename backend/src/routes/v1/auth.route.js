75e7e924e8b020e22fd3979986a8c69addd11b35
Dicki Maulana <bukandicki@gmail.com>
2024-10-30 23:37:42 +0700

    feat: login logout route

--- backend/src/routes/v1/auth.route.js
+++ backend/src/routes/v1/auth.route.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const { authController } = require("../../controllers");
 
 const router = express.Router();
 
-router.route("/").get().post();
+router
+    .route("/login")
+    .post(authController.login);
+
+router
+    .route("/logout")
+    .post(authController.logout);
+
 
 module.exports = router;
