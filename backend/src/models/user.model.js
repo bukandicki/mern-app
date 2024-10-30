0e75bae84821f3bc9d1b8f9ac879d02a32a80499
Dicki Maulana <bukandicki@gmail.com>
2024-10-30 16:45:39 +0700

    add pre save action to user schema

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+    const user = this
+
+    user.password = await bcrypt.hash(user.password, 8)
+    next()
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
