560ee2685e7e6d3e86569d0e948cb7fee4ba590e
Dicki Maulana <bukandicki@gmail.com>
2024-10-30 16:47:21 +0700

    chore: install body parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const routes = require("./routes/v1");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
+
+app.use(routes);
 
 mongoose
   .connect(
