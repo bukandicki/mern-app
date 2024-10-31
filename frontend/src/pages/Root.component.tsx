760a3f7d59bcbf8a5007e6d7bb605355f63001e7
Dicki Maulana <bukandicki@gmail.com>
2024-11-01 00:19:40 +0700

    feat: create root context

--- frontend/src/pages/Root.component.tsx
+++ frontend/src/pages/Root.component.tsx
@@ -1,20 +1,34 @@
 
-import { useEffect } from "react";
+import { createContext, Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import { UserType } from "../lib/types";
+
+export const AppContext = createContext<{ currentUser: UserType | null; setCurrentUser: Dispatch<SetStateAction<UserType | null>> }>({
+  currentUser: null,
+  setCurrentUser: () => {},
+});
 
 export default function Root() {
   const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState<UserType | null>(null);
   
   useEffect(() => {
-    if (localStorage.getItem("login") === "true") navigate("/dashboard");
-    else navigate("/login");
+    if (localStorage.getItem("user") !== null) {
+      navigate("/dashboard")
+      setCurrentUser(JSON.parse(localStorage.getItem("user") as string) as UserType);
+    }
+    else {
+      navigate("/login")
+    }
   }, []);
   
   return (
     <>
       <header></header>
-      <Outlet />
+      <AppContext.Provider value={{ currentUser, setCurrentUser }}>
+        <Outlet />
+      </AppContext.Provider>
       <footer></footer>
     </>
-  )
+  );
 }
